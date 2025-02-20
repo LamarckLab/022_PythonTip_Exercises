ae8e0669270d1dfe2cdc4350cce369dcb90a7776
L. Mingkai <704021302@qq.com>
2025-02-20 22:56:27 +0800

    Add files via upload

---
+++ 009.py
@@ -0,0 +1,13 @@
+def list_between(start, end):
+    # 此处写你的代码
+    my_list = []
+    for element in range(start+1,end):
+        my_list.append(element)
+    return my_list
+
+# 获取输入的start 及 end
+start = int(input())
+end = int(input())
+
+# 调用函数
+print(list_between(start, end))

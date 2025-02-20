1d629b0c2e08d4db69710614f1794f16dc5b4f5e
L. Mingkai <704021302@qq.com>
2025-02-20 22:30:42 +0800

    Add files via upload

---
+++ 007.py
@@ -0,0 +1,11 @@
+def is_plural(term):
+    # 此处编写代码 
+    if term[-1] == 's':
+        return True
+    else:
+        return False
+# 获取输入 
+input_word = input()
+
+# 调用函数并输出结果 
+print(is_plural(input_word))

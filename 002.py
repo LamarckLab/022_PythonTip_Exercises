34d1e0ddec71d35b35f417fedc7d72016c6d616a
L. Mingkai <704021302@qq.com>
2025-02-20 22:17:23 +0800

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,10 @@
+def convert_to_int(str_number):
+    # 在此处编写你的代码
+    return int(str_number)
+# 获取字符串输入
+input_string = input()
+
+result = convert_to_int(input_string)
+
+# 打印结果的类型
+print(type(result))

188cf3231581bceddde6662b088e13769c4194ce
L. Mingkai <704021302@qq.com>
2025-02-21 17:24:17 +0800

    Add files via upload

---
+++ 041.py
@@ -0,0 +1,9 @@
+def shared_chars_count(word1, word2):
+    # 此处编写代码
+    return len(set(word1) & set(word2))
+# 获取输入
+word1 = input()
+word2 = input()
+
+# 调用函数
+print(shared_chars_count(word1, word2))

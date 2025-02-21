f2e07b02d237fe634cb2a90696cb9d6c885b5e84
L. Mingkai <704021302@qq.com>
2025-02-21 14:31:00 +0800

    Update 025.py

--- 025.py
+++ 025.py
@@ -1,16 +1,12 @@
-def fibonacci_number(n):
-    # 在此处编写你的代码
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n >= 2:
-        target = fibonacci_number(n-1) + fibonacci_number(n-2)
-        return target
+def find_first_n_odds(n):
+    # 此处写你的代码 
+    my_list = []
+    for i in range(1, 2*n):
+        if i % 2 == 1:
+            my_list.append(i)
+    return my_list
 
-# 输入n的整数
+# 获取输入n
 n = int(input())
 # 调用函数
-print(fibonacci_number(n))
+print(find_first_n_odds(n))

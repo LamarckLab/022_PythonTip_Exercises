6ccdc89f45b9bdf8107fdfc662bf42c58707805d
L. Mingkai <704021302@qq.com>
2025-02-21 14:05:39 +0800

    Add files via upload

---
+++ 021.py
@@ -0,0 +1,13 @@
+def is_string_isogram(word):
+    # 将单词转换为小写
+    word = word.lower()
+    # 在此处编写你的代码
+    for element in word:
+        if word.count(element) > 1:
+            return False
+    return True
+
+# 从用户处获取输入
+word = input()
+# 调用函数
+print(is_string_isogram(word))

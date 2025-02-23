bd2e59969f629f77bae0f626447fdb2acc90169b
L. Mingkai <704021302@qq.com>
2025-02-23 15:25:57 +0800

    Add files via upload

---
+++ 061.py
@@ -0,0 +1,9 @@
+def find_common_elements(list1, list2):
+     # 此处编写代码 
+     return sorted(list(set(list1).intersection(set(list2))))
+# 获取用户输入，转换为列表
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+
+# 调用函数
+print(find_common_elements(list1, list2))

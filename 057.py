8bbd60ae557b809480a05b942e72b7780777196e
L. Mingkai <704021302@qq.com>
2025-02-23 16:01:32 +0800

    Add files via upload

--- 057.py
+++ 057.py
@@ -1,13 +1,11 @@
-def is_consecutive_sequence(num_list):
-    # 此处编写你的代码 
-    num_list = sorted(num_list)
-    flag = True
-    for index in range(0,len(num_list)-1):
-        if num_list[index+1] - num_list[index] != 1:
-            flag = False
-    return flag
-# 获取输入转为整数列表 
-nums = list(map(int, input().split()))
+def binary_to_int(bin_tuple):
+    # 此处编写代码，bin_tuple 为元组
+    target = 0
+    for cnt in range(0,len(bin_tuple)):
+        target += 2**cnt*bin_tuple[-cnt-1]
+    return target
+# 读取输入，将输入转换为元组 
+bin_tuple = tuple(map(int,input().strip().split()))
 
-# 调用函数 
-print(is_consecutive_sequence(nums))
+# 调用函数binary_to_int()，并输出结果
+print(binary_to_int(bin_tuple))

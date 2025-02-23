05348ec1e90d5c85cd0ffaa9a261746419fbec39
L. Mingkai <704021302@qq.com>
2025-02-23 15:48:07 +0800

    Add files via upload

---
+++ 065.py
@@ -0,0 +1,13 @@
+def merge_and_sort(first_list, second_list):
+    # 此处编写代码
+    if first_list[1] > first_list[0]:
+        return sorted(first_list+second_list)
+    else:
+        return sorted(first_list+second_list)[::-1]
+
+# 获取输入，转换为列表
+first_list = list(map(int, input().split()))
+second_list = list(map(int, input().split()))
+
+# 调用函数
+print(merge_and_sort(first_list, second_list))

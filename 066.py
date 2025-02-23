be3deb7c6e1d67a7b8bc7ca3673e376103acdaee
L. Mingkai <704021302@qq.com>
2025-02-23 16:05:44 +0800

    Add files via upload

---
+++ 066.py
@@ -0,0 +1,16 @@
+def is_string_palindrome(string):
+    # 此处编写代码
+    if len(string) < 2:
+        return True
+    else:
+        if string[0] == string[-1]:
+            string = string[1:-1]
+            return is_string_palindrome(string)
+        else:
+            return False
+
+# 获取用户输入
+user_input = input()
+
+# 调用函数
+print(is_string_palindrome(user_input.lower()))

b3bf7719aca3cbc05c2b888675ea00b8f40e23b0
L. Mingkai <704021302@qq.com>
2025-02-25 22:18:26 +0800

    Add files via upload

---
+++ 083.py
@@ -0,0 +1,14 @@
+def is_subset(sub_string, main_string):
+    # 在此处编写你的代码
+    flag = True
+    for char in sub_string:
+        if char not in main_string:
+            flag = False
+    return flag
+
+# 获取用户输入
+sub_string = input()
+main_string = input()
+
+# 调用函数
+print(is_subset(sub_string, main_string))

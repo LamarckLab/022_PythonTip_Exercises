067c0cded12bbea3d313ba5b96083f351ee3bfcd
L. Mingkai <704021302@qq.com>
2025-02-26 23:19:56 +0800

    Add files via upload

---
+++ 094.py
@@ -0,0 +1,18 @@
+def closest_vowel(letter):
+    # 此处编写代码
+    letter = letter.lower()
+    if letter <= 'c':
+        return 'a'
+    elif letter <= 'g':
+        return 'e'
+    elif letter <= 'l':
+        return 'i'
+    elif letter <= 'r':
+        return 'o'
+    else:
+        return 'u'
+
+# 获取输入
+letter = input()
+# 调用函数
+print(closest_vowel(letter))

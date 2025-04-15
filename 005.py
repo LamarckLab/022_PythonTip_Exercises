014e5f0ff369998265fe4d72cce1f334f26623c9
L. Mingkai <704021302@qq.com>
2025-04-15 21:52:27 +0800

    Update 005.py

--- 005.py
+++ 005.py
@@ -1,9 +1,6 @@
 def compare_length(str1, str2):
     # 此处编写你的代码 
-    if len(str1) == len(str2):
-        return True
-    else:
-        return False
+    return len(str1) == len(str2)
 
 # 获取输入 
 input_str1 = input()

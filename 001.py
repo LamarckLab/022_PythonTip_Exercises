c7500d16f985e843a1a87e08adba2e7378b657b5
L. Mingkai <704021302@qq.com>
2025-04-15 21:44:21 +0800

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,8 +1,9 @@
 def convert_to_seconds(minutes):
   # 在此处编写你的代码
-  return minutes*60
+  return 60*minutes
+
 # 输入分钟
 input_minutes = int(input())
 
-# 调用函数
+# 调用函数 
 print(convert_to_seconds(input_minutes))

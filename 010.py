ea397d0f5fa2d07f9dcef0f7163be0b30cea034c
L. Mingkai <704021302@qq.com>
2025-04-15 22:07:22 +0800

    Update 010.py

--- 010.py
+++ 010.py
@@ -1,10 +1,14 @@
 def check_prime(num):
     # 在此处编写代码
-    flag = True
-    for i in range(2, num):
-        if num % i == 0:
-            flag = False
-    return flag
+    if num == 2:
+        return True
+    else:
+        flag = True
+        for item in range(2,num):
+            if num % item == 0:
+                flag = False
+        return flag
+
 # 输入一个整数
 number = int(input())
 

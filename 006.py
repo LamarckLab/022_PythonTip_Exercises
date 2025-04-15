fed8a2cc713207a03fe147f0d9a6d936e373b1c7
L. Mingkai <704021302@qq.com>
2025-04-15 21:53:13 +0800

    Update 006.py

--- 006.py
+++ 006.py
@@ -1,8 +1,6 @@
 def join_first_last(input_str):
     # 在此处编写你的代码
-    new_str = input_str[0] + input_str[-1]
-    return new_str
-
+    return input_str[0] + input_str[-1]
 # 输入字符串
 given_string = input()
 

cf1afc261f96f8a1350c97189b847cfb0828b301
L. Mingkai <704021302@qq.com>
2025-04-16 15:39:03 +0800

    Update 013.py

--- 013.py
+++ 013.py
@@ -1,11 +1,11 @@
 def reverse_number_digits(num):
     # 在此处编写你的代码
-    my_list = []
+    result_list =[]
     while num > 0:
         digit = num % 10
-        my_list.append(digit)
+        result_list.append(digit)
         num //= 10
-    return my_list
+    return result_list
 
 # 获取用户输入
 num = int(input())

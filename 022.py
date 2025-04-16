fae2fd1c14f50228e9d6dd8bf9c52a81f75942cd
L. Mingkai <704021302@qq.com>
2025-04-16 16:46:29 +0800

    Update 022.py

--- 022.py
+++ 022.py
@@ -1,8 +1,6 @@
 def count_binary_ones(num):
     # 此处写你的代码
-    bin_num = bin(num)
-    str_num = str(bin_num)
-    return str_num.count("1")
+    return str(bin(num)).count("1")
 
 # 从标准输入读取一个整数
 num = int(input())

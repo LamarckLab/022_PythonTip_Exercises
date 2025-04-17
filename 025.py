626812859fd6fcddcff0e84d8283a6f7be0d0535
L. Mingkai <704021302@qq.com>
2025-04-17 15:03:12 +0800

    Update 025.py

--- 025.py
+++ 025.py
@@ -1,10 +1,11 @@
 def find_first_n_odds(n):
-    # 此处写你的代码 
+    # 此处写你的代码
     my_list = []
-    for i in range(1, 2*n):
-        if i % 2 == 1:
-            my_list.append(i)
+    for i in range(1,n+1):
+        digit = 2*i - 1
+        my_list.append(digit)
     return my_list
+
 
 # 获取输入n
 n = int(input())

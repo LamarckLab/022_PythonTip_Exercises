c6bf63d4adb3e61fd15f88ea170203c94b4b8751
L. Mingkai <704021302@qq.com>
2025-04-17 15:29:52 +0800

    Update 031.py

--- 031.py
+++ 031.py
@@ -2,9 +2,10 @@ def smallest_multiple(n):
     # 此处写你的代码
     target = n
     while True:
-        if all(target % i == 0 for i in range(1, n+1)):
+        if all(target % i == 0 for i in range(1,n+1)):
             return target
-        target += 1
+        else:
+            target += 1
 
 # 输入n
 n = int(input())

42912f52ac58eccf1f77266edc4bf57f3ce8c02b
L. Mingkai <704021302@qq.com>
2025-04-17 15:04:59 +0800

    Update 026.py

--- 026.py
+++ 026.py
@@ -1,14 +1,11 @@
 def fibonacci_number(n):
     # 在此处编写你的代码
-    if n <= 0:
+    if n == 0:
         return 0
     elif n == 1:
         return 1
-    elif n == 2:
-        return 1
-    elif n >= 2:
-        target = fibonacci_number(n-1) + fibonacci_number(n-2)
-        return target
+    else:
+        return fibonacci_number(n-1) + fibonacci_number(n-2)
 
 # 输入n的整数
 n = int(input())

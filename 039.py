eb0f46e30a7254d9c2c411570d846c4f7efeaf02
L. Mingkai <704021302@qq.com>
2025-04-17 23:16:27 +0800

    Update 039.py

--- 039.py
+++ 039.py
@@ -1,12 +1,8 @@
 def check_double_base_palindrome(number):
     # 此处编写代码
-    flag_10 = False
-    flag_2 = False
-    if str(number) == str(number)[::-1]:
-        flag_10 = True
-    if bin(number)[2:] == (bin(number)[2:])[::-1]:
-        flag_2 = True
-    return flag_10 and flag_2
+    str_num = str(number)
+    str_bin_num = bin(number)[2:]
+    return (str_num == str_num[::-1]) and (str_bin_num == str_bin_num[::-1])
 
 # 获取用户输入
 number = int(input())

71b9a0714f630523ff556e10fddaafb061be68f1
L. Mingkai <704021302@qq.com>
2025-04-17 23:02:02 +0800

    Update 038.py

--- 038.py
+++ 038.py
@@ -1,16 +1,16 @@
 def find_unique(lst):
     # 此处编写代码
-    cnt_dict = {}
-    unique_list = []
-    for element in lst:
-        if element not in cnt_dict:
-            cnt_dict[element] = 1
+    my_dict = {}
+    for item in lst:
+        if item not in my_dict:
+            my_dict[item] = 1
         else:
-            cnt_dict[element] += 1
-    for element in cnt_dict:
-        if cnt_dict[element] == 1:
-            unique_list.append(element)
-    return unique_list
+            my_dict[item] += 1
+    output_lst = []
+    for item in my_dict:
+        if my_dict[item] == 1:
+            output_lst.append(item)
+    return output_lst
 
 # 获取用户输入并转为数字列表
 numbers = list(map(int, input().split()))

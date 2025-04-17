b788c8f584472ddc6ffe6adec4432227ca90bd83
L. Mingkai <704021302@qq.com>
2025-04-17 15:31:12 +0800

    Update 032.py

--- 032.py
+++ 032.py
@@ -1,10 +1,6 @@
 def find_highest_number(numbers_list):
-    # 此处编写代码 
-    max_num = 0
-    for element in numbers_list:
-        max_num = max(max_num, element)
-    return max_num
-
+    # 此处编写代码
+    return max(numbers_list)
 
 # 输入数字并转为列表
 numbers_list = list(map(int, input().split()))

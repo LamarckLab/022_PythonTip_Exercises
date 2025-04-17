59548f8c0b99d491f879cf912c5f479867d53ff3
L. Mingkai <704021302@qq.com>
2025-04-17 15:17:54 +0800

    Update 028.py

--- 028.py
+++ 028.py
@@ -1,13 +1,13 @@
 def get_sorted_keys_values(dict_obj):
     # 此处写你的代码
     key_list = []
-    for element in dict_obj:
-        key_list.append(element)
-    sorted_list = sorted(key_list)
     value_list = []
-    for element in sorted_list:
-        value_list.append(dict_obj[element])
-    return [sorted_list,value_list]
+    for item in dict_obj:
+        key_list.append(item)
+    key_list = sorted(key_list)
+    for item in key_list:
+        value_list.append(dict_obj[item])
+    return [key_list,value_list]
 
 # 获取用户输入转为字典
 dictionary = eval(input())

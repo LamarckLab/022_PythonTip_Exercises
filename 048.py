8d282c3798e335b0f189152b99eed044ae6aafe6
L. Mingkai <704021302@qq.com>
2025-04-18 15:50:36 +0800

    Update 048.py

--- 048.py
+++ 048.py
@@ -1,11 +1,10 @@
 def count_sublists(list_input):
     # 此处编写代码
-    cnt = 0
-    for element in list_input:
-        if type(element) == list:
-            cnt += 1
-    return cnt
-
+    count = 0
+    for sub_lst in list_input:
+        if type(sub_lst) == list:
+            count += 1
+    return count
 # 获取输入转为列表 
 list_input = eval(input())
 

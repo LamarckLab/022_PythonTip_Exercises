d3ab5b9c177fc8fe5ccc40e54410114677f2f4fa
L. Mingkai <704021302@qq.com>
2025-04-18 23:00:46 +0800

    Update 058.py

--- 058.py
+++ 058.py
@@ -1,10 +1,13 @@
 def reverse_binary_integer(n):
     # 此处编写你的代码
-    n_bin = bin(n)[2:]
-    n_bin_reversed = n_bin[::-1]
-    return int(n_bin_reversed, 2)
-
-
+    bin_n = bin(n)[2:]
+    reversed_bin_n = bin_n[::-1]
+    exponent = 0
+    reversed_decimal_n = 0
+    for digit in reversed_bin_n[::-1]:
+        reversed_decimal_n += int(digit) * (2**exponent)
+        exponent += 1
+    return reversed_decimal_n
 # 获取输入 
 n = int(input())
 

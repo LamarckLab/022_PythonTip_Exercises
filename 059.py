8b29a4fa1b6fe95fe1f2fade2868083777f56674
L. Mingkai <704021302@qq.com>
2025-04-18 23:06:27 +0800

    Update 059.py

--- 059.py
+++ 059.py
@@ -1,26 +1,24 @@
 def count_consonants(word):
     # 在此处编写你的代码
-    consonants_cnt = 0
-    word = word.lower()
-    for element in word:
-        if element in consonants:
-            consonants_cnt += 1
-    return consonants_cnt
+    consonants_str ="bcdfghjklmnpqrstvwxyz"
+    consonants_count = 0
+    for letter in word.lower():
+        if letter in consonants_str:
+            consonants_count += 1
+    return consonants_count
 
 def count_vowels(word):
     # 在此处编写你的代码
-    vowels_cnt = 0
-    word = word.lower()
-    for element in word:
-        if element in vowels:
-            vowels_cnt += 1
-    return vowels_cnt
+    vowels_str = "aeiou"
+    vowels_count = 0
+    for letter in word.lower():
+        if letter in vowels_str:
+            vowels_count += 1
+    return vowels_count
 
 # 获取用户输入
 word = input()
 
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
 # 调用函数
 print(count_consonants(word))
 print(count_vowels(word))

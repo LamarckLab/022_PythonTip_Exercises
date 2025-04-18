26e43c9359caf1b371c2349d5655c6f8067e1ffc
L. Mingkai <704021302@qq.com>
2025-04-18 22:52:00 +0800

    Update 055.py

--- 055.py
+++ 055.py
@@ -1,12 +1,11 @@
 def binary_to_decimal(binary_list):
-     # 此处编写你的代码
-     cnt = 1
-     target = 0
-     while cnt < len(binary_list)+1:
-        target += 2**(cnt-1)*binary_list[-cnt]
-        cnt += 1
-     return target
-
+     # 此处编写你的代码 
+     exponent = 0
+     decimal = 0
+     for digit in binary_list[::-1]:
+          decimal += digit * (2**exponent)
+          exponent += 1
+     return decimal
 # 获取输入，并转为列表 
 binary_list = list(map(int, input().split()))
 

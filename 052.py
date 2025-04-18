2f2662eb2268774b2be3aa59f2b5c6be6c10ee68
L. Mingkai <704021302@qq.com>
2025-04-18 16:47:49 +0800

    Update 052.py

--- 052.py
+++ 052.py
@@ -1,15 +1,14 @@
 def get_longest_word(sentence):
     # 在此处编写你的代码
-    target_dict = {}
+    sentence_lst = sentence.split(" ")
     max_len = 0
-    sentence_list = sentence.split(" ")
-    for element in sentence_list:
-        target_dict[element] = len(element)
-        if len(element) > max_len:
-            max_len = len(element)
-    for element in target_dict:
-        if target_dict[element] == max_len:
-            return element
+    longest_word = ""
+    for word in sentence_lst:
+        if len(word) > max_len:
+            max_len = len(word)
+            longest_word = word
+    return longest_word
+
 
 # 获取输入 
 sentence = input()

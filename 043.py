ea2d576e5d345a6cefaeb8bf98d271413dec714f
L. Mingkai <704021302@qq.com>
2025-04-18 15:32:32 +0800

    Update 043.py

--- 043.py
+++ 043.py
@@ -1,15 +1,12 @@
 import datetime
 
 def calculate_days_between(date1, date2):
-    # 在此处编写你的代码
-    date_format = "%Y-%m-%d" # 日期格式
-    d1 = datetime.datetime.strptime(date1, date_format) # 将字符串转换为datetime对象
+    date_format = "%Y-%m-%d"
+    d1 = datetime.datetime.strptime(date1, date_format) 
     d2 = datetime.datetime.strptime(date2, date_format)
-    difference = d2 - d1 # 计算两个日期之间的差异
-    return difference.days # 以天数的差异作为输出返回
-# 获取用户输入
+    difference = d2 - d1 
+    return difference.days 
+
 date1 = input()
 date2 = input()
-
-# 调用函数
 print(calculate_days_between(date1, date2))

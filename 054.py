3e9fe4d745d99475b36fe6583c2535c5e2e49d2b
L. Mingkai <704021302@qq.com>
2025-04-18 22:47:45 +0800

    Update 054.py

--- 054.py
+++ 054.py
@@ -2,15 +2,14 @@ def is_sum_even(string):
     # 此处编写你的代码
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     index_sum = 0
-    for element in string:
-        if element in alphabet:
-            index_sum += alphabet.index(element) + 1
-    if index_sum % 2 == 0:
-        return True
-    else:
-        return False
+    string = string.lower()
+    for letter in string:
+        if letter in alphabet:
+            index_sum += alphabet.index(letter) + 1
+    return index_sum % 2 == 0
+
 # 获取字符串 
 string = input()
 
 # 调用函数 
-print(is_sum_even(string))s
+print(is_sum_even(string))

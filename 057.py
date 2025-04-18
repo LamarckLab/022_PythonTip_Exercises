f7dba3ee22c7fe2dd942b913b658fbf8264c32d8
L. Mingkai <704021302@qq.com>
2025-04-18 22:57:18 +0800

    Update 057.py

--- 057.py
+++ 057.py
@@ -1,11 +1,13 @@
 def is_consecutive_sequence(num_list):
-    # 此处编写你的代码 
+    # 此处编写你的代码
     num_list = sorted(num_list)
     flag = True
-    for index in range(0,len(num_list)-1):
+    for index in range(0, len(num_list)-1):
         if num_list[index+1] - num_list[index] != 1:
             flag = False
     return flag
+
+
 # 获取输入转为整数列表 
 nums = list(map(int, input().split()))
 

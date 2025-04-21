9399bb78c54085265ce964e5906fffdde9a22857
L. Mingkai <704021302@qq.com>
2025-04-21 16:34:30 +0800

    Update 071.py

--- 071.py
+++ 071.py
@@ -1,13 +1,11 @@
 def smallest_multiple(digits, multiple_of):
     # 此处编写代码
-    num_start = 10**(digits-1)
-    flag = True
-    while flag:
-        if num_start % multiple_of == 0:
-            flag = False
-            return num_start
+    num = 10**(digits-1)
+    while True:
+        if num % multiple_of == 0:
+            return num
         else:
-            num_start += 1
+            num += 1
 
 # 获取输入 
 digits = int(input())

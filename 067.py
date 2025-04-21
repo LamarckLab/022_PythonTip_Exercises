2978b790ea4de661330f43f28afae0cdf8d1cd0f
L. Mingkai <704021302@qq.com>
2025-04-21 16:00:01 +0800

    Update 067.py

--- 067.py
+++ 067.py
@@ -1,9 +1,10 @@
 def binary_to_int(bin_tuple):
     # 此处编写代码，bin_tuple 为元组
-    target = 0
-    for cnt in range(0,len(bin_tuple)):
-        target += 2**cnt*bin_tuple[-cnt-1]
-    return target
+    decimal_output = 0
+    for i in range(len(bin_tuple)):
+        decimal_output += bin_tuple[-i-1] * (2**i)
+    return decimal_output
+
 # 读取输入，将输入转换为元组 
 bin_tuple = tuple(map(int,input().strip().split()))
 

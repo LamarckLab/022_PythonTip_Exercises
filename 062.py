b0bfba758fe09de579fe582ee000fc7b065fa69b
L. Mingkai <704021302@qq.com>
2025-04-21 11:44:48 +0800

    Update 062.py

--- 062.py
+++ 062.py
@@ -1,14 +1,8 @@
 def is_date_palindromic(date_in_string):
     # 此处编写你的代码
-    flag1 = True
-    flag2 = True
-    str1 = date_in_string[:2] + date_in_string[3:5] + date_in_string[6:10]
-    str2 = date_in_string[3:5] + date_in_string[:2] + date_in_string[6:10]
-    if str1 != str1[::-1]:
-        flag1 = False
-    if str2 != str2[::-1]:
-        flag2 = False
-    return flag1 and flag2
+    date_ddmmyyyy = date_in_string[:2] + date_in_string[3:5] + date_in_string[6:]
+    date_mmddyyyy = date_in_string[3:5] + date_in_string[:2] + date_in_string[6:]
+    return date_ddmmyyyy == date_ddmmyyyy[::-1] and date_mmddyyyy == date_mmddyyyy[::-1]
 
 # 获取日期输入 
 date_in_string = input()

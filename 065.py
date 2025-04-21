7562186245e97d8176b74a6c2b08c98acb3f3450
L. Mingkai <704021302@qq.com>
2025-04-21 15:46:59 +0800

    Update 065.py

--- 065.py
+++ 065.py
@@ -1,9 +1,9 @@
 def merge_and_sort(first_list, second_list):
     # 此处编写代码
-    if first_list[1] > first_list[0]:
-        return sorted(first_list+second_list)
+    if first_list == sorted(first_list):
+        return sorted(first_list + second_list)
     else:
-        return sorted(first_list+second_list)[::-1]
+        return sorted(first_list + second_list)[::-1]
 
 # 获取输入，转换为列表
 first_list = list(map(int, input().split()))

8a892b2cbdf815497cb1253ff25e0e17905ca6cf
L. Mingkai <704021302@qq.com>
2025-04-21 16:50:33 +0800

    Update 072.py

--- 072.py
+++ 072.py
@@ -1,16 +1,13 @@
 def sort_by_last_char(sentence):
      # 此处编写代码
-     sentence_list = sentence.split(" ")
-     for index in range(len(sentence_list)):
-        sentence_list[index] = sentence_list[index][::-1]
-     sentence_list = sorted(sentence_list)
-     for index in range(len(sentence_list)):
-        sentence_list[index] = sentence_list[index][::-1]
-     target = ""
-     for element in sentence_list:
-        target += (element+" ")
-     return target[:-1]
-
+     words_lst = sentence.split(" ")
+     for i in range(len(words_lst)):
+          words_lst[i] = words_lst[i][::-1]
+     words_lst = sorted(words_lst)
+     for i in range(len(words_lst)):
+          words_lst[i] = words_lst[i][::-1]
+     return " ".join(words_lst)
+     
 # 输入句子 
 sentence = input()
 

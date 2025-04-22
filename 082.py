51c5062ce2179bb397cdd8c3146668b69e7d0df3
L. Mingkai <704021302@qq.com>
2025-04-22 23:47:45 +0800

    Update 082.py

--- 082.py
+++ 082.py
@@ -1,18 +1,14 @@
 def is_heterogram(s):
     # 此处编写代码
-    s = s.replace(" ","")
-    target_dict = {}
-    for element in s:
-        if element not in target_dict:
-            target_dict[element] = 1
-        else:
-            target_dict[element] += 1
-    for element in target_dict:
-        if target_dict[element] > 1:
-            return "No"
-    return "Yes"
-
-
+    extract_s = s.replace(" ","")
+    flag = True
+    for element in extract_s:
+        if extract_s.count(element) > 1:
+            flag = False
+    if flag:
+        return "Yes"
+    else:
+        return "No"
 
 # 获取输入 
 input_string = input()

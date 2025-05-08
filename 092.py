cc4e08686f654fb9efa7825691eb75aa2d09de3b
L. Mingkai <704021302@qq.com>
2025-05-08 22:43:32 +0800

    Update 092.py

--- 092.py
+++ 092.py
@@ -1,29 +1,10 @@
 def can_form_palindrome(word):
     # 在此处编写你的代码
-    count_dict = {}
-    count_odd = 0
-    flag = True
-    if len(word) % 2 == 0:
-        for char in word:
-            if char not in count_dict:
-                count_dict[char] = 1
-            else:
-                count_dict[char] += 1
-        for element in count_dict:
-            if count_dict[element] % 2 != 0:
-                flag = False
-    else:
-        for char in word:
-            if char not in count_dict:
-                count_dict[char] = 1
-            else:
-                count_dict[char] += 1
-        for element in count_dict:
-            if count_dict[element] % 2 != 0:
-                count_odd += 1
-        if count_odd > 1:
-            flag = False
-    return flag
+    odd_count = 0
+    for letter in word:
+        if word.count(letter) % 2 == 1:
+            odd_count += 1
+    return odd_count <= 1
 
 # 从用户处获取输入
 word = input()

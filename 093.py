1458eb9604960e73b8f287091d2f515a4e395a85
L. Mingkai <704021302@qq.com>
2025-05-08 22:58:08 +0800

    Update 093.py

--- 093.py
+++ 093.py
@@ -1,24 +1,12 @@
 def list_into_chunks(num_list, chunk_size):    
     # 此处编写代码
-    index = 0
-    sub_list = []
-    target_list =[]
-    i = 0
-    while i < len(num_list):
-        index += 1
-        if index <= chunk_size:
-            sub_list += [num_list[i]]
-            i += 1
-        else:
-            index = 0
-            target_list.append(sub_list)
-            sub_list = []
-    if index > 0:
-        target_list.append(sub_list)
-    return target_list
+    output_list = []
+    start_index = 0
+    while start_index < len(num_list):
+        output_list.append(num_list[start_index:start_index+chunk_size])
+        start_index += chunk_size
+    return output_list
 
-    
-    
 # 从用户输入中获取数据，并将其转换为列表
 num_list = list(map(int, input().split()))
 # 从用户输入中获取块大小

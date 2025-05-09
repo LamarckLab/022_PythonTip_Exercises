a4057e8e86ea512f396b33153c9b56a1ccc49763
L. Mingkai <704021302@qq.com>
2025-05-09 16:28:03 +0800

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,17 +1,14 @@
 def break_down_list(items):
     # 此处编写代码
-    sub_dict = {}
-    target_list = []
-    while len(items) > 0:
-        if items[0]["quantity"] > 0:
-            sub_dict["type"] = items[0]["type"]
-            sub_dict["quantity"] = 1
-            target_list += [sub_dict]
-            sub_dict = {}
-            items[0]["quantity"] -= 1
-        else:
-            items = items[1:]
-    return target_list
+    output_list = []
+    for sub_dict in items:
+        while sub_dict['quantity'] > 0:
+            temp_dict = sub_dict.copy()
+            temp_dict['quantity'] = 1
+            output_list.append(temp_dict)
+            sub_dict['quantity'] -= 1
+    return output_list
+
 # 获取物品列表 
 items = eval(input())
 # 调用函数，输出分解后的物品列表

643ac47c38f624b3c1cb50030a00947532bd5ebb
L. Mingkai <704021302@qq.com>
2025-05-09 11:22:26 +0800

    Update 095.py

--- 095.py
+++ 095.py
@@ -1,14 +1,13 @@
 def missing_numbers_info(num_list):
     # 此处写下你的代码
-    max_num = max(num_list)
-    min_num = min(num_list)
-    target_lst = []
-    sum_num = 0
-    for digit in range(min_num,max_num+1):
-        if digit not in num_list:
-            target_lst += [digit]
-            sum_num += digit
-    return (len(target_lst), sum_num)
+    sorted_list = sorted(num_list)
+    start, end = sorted_list[0], sorted_list[-1]
+    output_list = [0,0]
+    for num in range(start, end+1):
+        if num not in num_list:
+            output_list[0] += 1
+            output_list[1] += num
+    return tuple(output_list)
 
 # 获取整数输入并将其转换为列表
 num_list = list(map(int, input().split()))

ac5a5eea74fc5a48c45e41ad328e05280027d462
L. Mingkai <704021302@qq.com>
2025-05-09 13:55:39 +0800

    Update 097.py

--- 097.py
+++ 097.py
@@ -1,13 +1,9 @@
 def get_all_permutations(digits):
     # 在此处编写你的代码
-    target_list = []
-    index_sort = ["012","021","102","120","201","210"]
-    for i in range(0,6):
-        for j in range(0,3):
-            target_list += [digits[int(index_sort[i][j])]]
-        print(target_list[0], target_list[1], target_list[2])
-        target_list = []
-            
+    pmtt_list = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,0,1],[2,1,0]]
+    for pmtt in pmtt_list:
+        print(digits[pmtt[0]], digits[pmtt[1]], digits[pmtt[2]])
+    return None
 
 # 获取整数输入并将其转换为列表
 digits = list(map(int, input().split()))

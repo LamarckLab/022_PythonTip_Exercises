5d550bbf93477360e0a8a5f47ff0b0285a4e27d3
L. Mingkai <704021302@qq.com>
2025-05-09 16:09:56 +0800

    Update 099.py

--- 099.py
+++ 099.py
@@ -1,10 +1,10 @@
 # 定义函数
 def extract_max_min(input_tuple, k):
-    # 对输入元组进行排序
-    sorted_tuple = sorted(input_tuple)
     # 在此处编写你的代码
-    return tuple(sorted_tuple[:k] + sorted_tuple[-k:])
-
+    input_list = sorted(list(input_tuple))
+    output_list = input_list[:k] + input_list[len(input_list)-k:]
+    return tuple(output_list)
+    
 # 获取整数输入并将其转换为元组
 input_tuple = tuple(map(int, input().split()))
 # 获取K的值

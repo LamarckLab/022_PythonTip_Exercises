8062ba6515f9ed465f969502d907218d24461e5a
L. Mingkai <704021302@qq.com>
2025-05-09 14:37:07 +0800

    Update 098.py

--- 098.py
+++ 098.py
@@ -1,11 +1,9 @@
 def binary_anagram(num1, num2):
     # 此处编写代码
-    bin1 = bin(num1)[2:]
-    bin2 = bin(num2)[2:]
-    if bin1.count("1") == bin2.count("1"):
-        return True
-    else:
-        return False
+    bin_1 = bin(num1)[2:]
+    bin_2 = bin(num2)[2:]
+    return bin_1.count("1") == bin_2.count("1")
+
 # 获取输入 
 num1 = int(input())
 num2 = int(input())

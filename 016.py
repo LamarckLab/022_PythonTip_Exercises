d9788e9423df7cf3529cbec5e1d25d9176503392
L. Mingkai <704021302@qq.com>
2025-08-29 22:29:39 +0800

    Update 016.py

--- 016.py
+++ 016.py
@@ -1,12 +1,10 @@
 def is_product_divisible_by_sum(numbers_list):
     # 此处编写代码
-    my_product = 1
-    my_sum = 0
-    for item in numbers_list:
-        my_product *= item
-        my_sum += item
-    return (my_product % my_sum) == 0
-
+    list_product, list_sum = 1, 0
+    for element in numbers_list:
+        list_product *= element
+        list_sum += element
+    return list_product % list_sum == 0
 # 获取整数输入并将其转换为列表
 numbers_list = list(map(int, input().split()))
 # 调用函数打印结果

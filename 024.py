7eb3ae10fcf9fa49b17e14dc08572aa250e5eec4
L. Mingkai <704021302@qq.com>
2025-08-29 23:07:54 +0800

    Update 024.py

--- 024.py
+++ 024.py
@@ -1,11 +1,10 @@
 def find_even_numbers(num):
     # 此处写入代码
-    my_list = []
-    for item in range(2, num+1):
-        if item % 2 == 0:
-            my_list.append(item)
-    return my_list
-
+    even_list = []
+    for element in range(2,num+1):
+        if element % 2 == 0:
+            even_list.append(element)
+    return even_list
 # 获取整数输入
 num = int(input())
 # 调用函数

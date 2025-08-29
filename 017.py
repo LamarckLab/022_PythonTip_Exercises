1dfc07f340cb20d7d56f51cb9ff14cba22d89a25
L. Mingkai <704021302@qq.com>
2025-08-29 22:37:07 +0800

    Update 017.py

--- 017.py
+++ 017.py
@@ -1,11 +1,10 @@
 def find_nth_smallest(numbers_list, n):
     # 此处编写你的代码
     sorted_list = sorted(numbers_list)
-    if n <= len(sorted_list):
+    if n <= len(numbers_list):
         return sorted_list[n-1]
     else:
         return None
-
 # 将输入的整数转换为列表
 numbers_list = list(map(int, input().split()))
 # 获取n的输入

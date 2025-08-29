98c892fb94fcdfae11250a67aa5d2ada9d8014b9
L. Mingkai <704021302@qq.com>
2025-08-29 22:23:32 +0800

    Update 015.py

--- 015.py
+++ 015.py
@@ -1,8 +1,6 @@
 def is_string_identical(text_string):
     # 此处编写代码
-    my_set = set(text_string)
-    return len(my_set) == 1
-
+    return len(set(text_string)) == 1
 # 获取输入 
 text_string = input()
 # 调用函数 

6a2d51e348efc8f161038c0f0981ea628b7d6bdd
L. Mingkai <704021302@qq.com>
2025-08-29 22:57:28 +0800

    Update 021.py

--- 021.py
+++ 021.py
@@ -2,12 +2,10 @@ def is_string_isogram(word):
     # 将单词转换为小写
     word = word.lower()
     # 在此处编写你的代码
-    my_list = []
-    for item in word:
-        my_list.append(item)
-    my_set = set(my_list)
-    return len(my_list) == len(my_set)
-
+    for element in word:
+        if word.count(element) > 1:
+            return False
+    return True
 # 从用户处获取输入
 word = input()
 # 调用函数

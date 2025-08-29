270cb7033ef90b9caf436be8cbcd95ab41cafa19
L. Mingkai <704021302@qq.com>
2025-08-29 22:45:03 +0800

    Update 020.py

--- 020.py
+++ 020.py
@@ -2,11 +2,10 @@ def multiply_numbers_in_string(num_string):
     # 将字符串输入转换为列表
     num_list = list(map(int, num_string.split()))
     # 在此处编写你的代码
-    my_product = 1
-    for item in num_list:
-        my_product *= item
-    return my_product
-
+    product = 1
+    for element in num_string.replace(" ",""):
+        product *= int(element)
+    return product
 # 获取输入字符串
 num_string = input()
 # 调用函数

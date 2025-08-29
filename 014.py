8012ab3b94d148ff6907bd8f845a6dd8773d766b
L. Mingkai <704021302@qq.com>
2025-08-29 22:21:33 +0800

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,9 +1,8 @@
 def are_anagrams(string1, string2):
     # 此处编写代码
-    list1 = string1.lower().replace(" ","")
-    list2 = string2.lower().replace(" ","")
-    return sorted(list1) == sorted(list2)
-
+    string1 = string1.lower().replace(" ","")
+    string2 = string2.lower().replace(" ","")
+    return sorted(string1) == sorted(string2)
 # 获取输入string1 和 string2 
 string1 = input()
 string2 = input()

e8759b476ca8c1bb20f59ba5c4b8be9d65080811
L. Mingkai <704021302@qq.com>
2025-08-29 22:40:51 +0800

    Update 018.py

--- 018.py
+++ 018.py
@@ -1,9 +1,8 @@
 def find_gcd(a, b):
     # 在此处编写代码
-    gcd = 1
-    for item in range(1,min(a,b)+1):
-        if (a%item == 0) & (b%item == 0):
-            gcd = item
+    for i in range(1,min(a,b)+1):
+        if a%i == 0 and b%i == 0:
+            gcd = i
     return gcd
 # 输入整数
 first_number = int(input())

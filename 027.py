51d99479cf18472601735e7f02d97c1283f146dd
L. Mingkai <704021302@qq.com>
2025-08-31 15:57:28 +0800

    Update 027.py

--- 027.py
+++ 027.py
@@ -1,7 +1,7 @@
 def reverse_sentence_words(sentence):
     # 此处写你的代码
-    my_list = sentence.split(" ")[::-1]
-    return " ".join(my_list)
+    words_list = sentence.split(" ")[::-1]
+    return " ".join(words_list)
 # 获取输入
 sentence = input()
 # 调用函数并打印结果

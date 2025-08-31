07f8bd7b9291260e9884cabacee1b66cbfff142b
L. Mingkai <704021302@qq.com>
2025-08-31 21:39:36 +0800

    Update 044.py

--- 044.py
+++ 044.py
@@ -1,11 +1,9 @@
 def shift_char(word):
-    # 此处编写你的代码 
-    ouput_word = ""
-    for digit in word:
-        digit = chr(ord(digit)+1)
-        ouput_word += digit
-    return ouput_word
-
+    # 此处编写你的代码
+    output_word = ""
+    for letter in word:
+        output_word += chr(ord(letter)+1)
+    return output_word
 # 获取单词
 word = input()
 # 调用函数 

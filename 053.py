051fc323afb075de6bbe8231cdb78d147d22fd7e
L. Mingkai <704021302@qq.com>
2025-08-31 22:42:37 +0800

    Update 053.py

--- 053.py
+++ 053.py
@@ -1,11 +1,10 @@
 def check_perfect(num):
     # 此处编写你的代码
     factor_sum = 0
-    for i in range(1, num):
+    for i in range(1,num):
         if num % i == 0:
             factor_sum += i
     return factor_sum == num
-
 # 输入处理 
 num = int(input())
 

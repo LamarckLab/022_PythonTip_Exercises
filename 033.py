8a89de17f0216d40010a775f97394c45d92d40c6
L. Mingkai <704021302@qq.com>
2025-08-31 16:32:56 +0800

    Update 033.py

--- 033.py
+++ 033.py
@@ -1,14 +1,12 @@
 def calculate_sum(numbers_list):
     # 此处编写代码
-    odd_sum = 0
-    even_sum = 0
-    for item in numbers_list:
-        if item % 2 == 0:
-            even_sum += item
+    odd_sum, even_sum = 0, 0
+    for digit in numbers_list:
+        if digit % 2 == 0:
+            even_sum += digit
         else:
-            odd_sum += item
-    return [even_sum,odd_sum]
-
+            odd_sum += digit
+    return [even_sum, odd_sum]
 # 获取输入转为列表
 numbers_list = list(map(int,input().split()))
 

9f05fa5b39b201afb6dd8003e7cc882ec2edfd51
L. Mingkai <704021302@qq.com>
2025-08-31 21:49:51 +0800

    Update 046.py

--- 046.py
+++ 046.py
@@ -1,13 +1,10 @@
 def dict_to_sorted_list(dictionary):
     # 此处编写你的代码
-    key_lst = []
-    output_lst = []
-    for key in dictionary:
-        key_lst.append(key)
-    for element in sorted(key_lst):
-        output_lst.append([element,dictionary[element]])
-    return output_lst
-
+    output_list = []
+    for key in sorted(dictionary):
+        sublist = [key, dictionary[key]]
+        output_list.append(sublist)
+    return output_list
 # 获取输入转为字典
 dictionary = eval(input())
 

359594ae77f032fae502dccde66bbd290120f466
L. Mingkai <704021302@qq.com>
2025-08-31 22:50:56 +0800

    Update 056.py

--- 056.py
+++ 056.py
@@ -2,10 +2,9 @@ def find_largest_even(lst):
     # 此处编写你的代码
     max_even = -1
     for digit in lst:
-        if digit % 2 == 0:
-            max_even = max(max_even, digit)
+        if (digit % 2) == 0 and (digit > max_even):
+            max_even = digit
     return max_even
-
 # 获取输入转为整数列表 
 input_list = list(map(int, input().split()))
 

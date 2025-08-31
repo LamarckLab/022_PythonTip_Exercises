feee4345719f2f5d515a13062dc2dc999f07c041
L. Mingkai <704021302@qq.com>
2025-08-31 16:57:59 +0800

    Update 037.py

--- 037.py
+++ 037.py
@@ -1,12 +1,10 @@
 def is_title(sentence):
     # 在此处编写代码
-    my_list = sentence.split(" ")
-    flag = True
-    for element in my_list:
-        if element[0].islower():
-            flag = False
-    return flag
-
+    word_list = sentence.split(" ")
+    for word in word_list:
+        if word[0].islower():
+            return False
+    return True  
 # 从用户处获取输入
 input_sentence = input()
 # 调用函数

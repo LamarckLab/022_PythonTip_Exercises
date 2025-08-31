2942cc034f07e550ee5fd7895bfa76ca3e23351b
L. Mingkai <704021302@qq.com>
2025-08-31 21:12:43 +0800

    Update 040.py

--- 040.py
+++ 040.py
@@ -1,11 +1,10 @@
 def find_indices(my_list, element):
     # 此处编写代码
-    output_lst =[]
-    for i in range(0, len(my_list)):
-        if my_list[i] == element:
-            output_lst.append(i)
-    return output_lst
-
+    index_list = []
+    for index in range(len(my_list)):
+        if element == my_list[index]:
+            index_list.append(index)
+    return index_list
 # split()函数将输入的字符串分割成一个列表
 user_input = input().split()
 element = input()

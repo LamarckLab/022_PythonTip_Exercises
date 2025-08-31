7ffbdb454f517a05c4f560f4e55060adef38606b
L. Mingkai <704021302@qq.com>
2025-08-31 22:01:30 +0800

    Update 047.py

--- 047.py
+++ 047.py
@@ -1,12 +1,9 @@
 def get_missing_letters(word_string):
-    # 此处编写你的代码
-    full_str = "abcdefghijklmnopqrstuvwxyz"
-    output_str = ""
-    for element in full_str:
-        if element not in word_string:
-            output_str += element
-    return output_str
-
+    # 此处编写你的代码 
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in word_string:
+        alphabet = alphabet.replace(letter,"")
+    return alphabet
 # 获取输入的字符串 
 word_string = input()
 

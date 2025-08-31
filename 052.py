1caf706ffb2bbfac54c0dac6f9974006c94fa916
L. Mingkai <704021302@qq.com>
2025-08-31 22:37:20 +0800

    Update 052.py

--- 052.py
+++ 052.py
@@ -1,15 +1,11 @@
 def get_longest_word(sentence):
     # 在此处编写你的代码
-    sentence_lst = sentence.split(" ")
-    max_len = 0
+    word_list = sentence.split(" ")
     longest_word = ""
-    for word in sentence_lst:
-        if len(word) > max_len:
-            max_len = len(word)
+    for word in word_list:
+        if len(word) > len(longest_word):
             longest_word = word
     return longest_word
-
-
 # 获取输入 
 sentence = input()
 

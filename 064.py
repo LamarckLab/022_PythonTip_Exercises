980d6a391251e04965a1bcfde5f54854cf5e9ff6
L. Mingkai <704021302@qq.com>
2025-09-01 14:39:40 +0800

    Update 064.py

--- 064.py
+++ 064.py
@@ -1,10 +1,6 @@
 def common_letters(word1, word2):
     # 此处编写代码
-    common_set = set(word1).intersection(set(word2))
-    common_str = ""
-    for letter in sorted(common_set):
-        common_str += letter
-    return common_str
+    return "".join(sorted(set(word1) & set(word2)))
 # 输入两个单词
 word1 = input()
 word2 = input()

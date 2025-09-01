b5fa69a89072d0f90d9513ebec277f509d535c3a
L. Mingkai <704021302@qq.com>
2025-09-01 11:43:14 +0800

    Update 062.py

--- 062.py
+++ 062.py
@@ -2,8 +2,7 @@ def is_date_palindromic(date_in_string):
     # 此处编写你的代码
     date_ddmmyyyy = date_in_string[:2] + date_in_string[3:5] + date_in_string[6:]
     date_mmddyyyy = date_in_string[3:5] + date_in_string[:2] + date_in_string[6:]
-    return date_ddmmyyyy == date_ddmmyyyy[::-1] and date_mmddyyyy == date_mmddyyyy[::-1]
-
+    return date_ddmmyyyy == date_mmddyyyy
 # 获取日期输入 
 date_in_string = input()
 

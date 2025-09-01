18ad3fbb5a4013b9b22f3a5016560afd572f7345
L. Mingkai <704021302@qq.com>
2025-09-01 14:53:02 +0800

    Update 069.py

--- 069.py
+++ 069.py
@@ -1,11 +1,10 @@
 def is_sum_present(num_list, target_sum):
     # 此处编写代码
-    for letter1 in num_list:
-        for letter2 in num_list:
-            if letter1 + letter2 == target_sum and letter1 != letter2:
+    for i in range(len(num_list)-1):
+        for j in range(i+1,len(num_list)):
+            if num_list[i] + num_list[j] == target_sum:
                 return True
     return False
-
 # 获取输入
 num_list = list(map(int, input().split()))
 target_sum = int(input())

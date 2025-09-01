93f0a35df10fb150ea44cd8860d77c5941111fdb
L. Mingkai <704021302@qq.com>
2025-09-01 21:29:48 +0800

    Update 081.py

--- 081.py
+++ 081.py
@@ -1,20 +1,18 @@
 def check_consecutive_sum(n):
     # 此处写代码
-    for i in range(1, n):
-        sum = i
-        for j in range(i+1, n):
-            sum += j
-            if sum > n:
-                break
-            elif sum < n:
-                continue
-            else:
+    sum_temp = 0
+    for i in range(1,n):
+        sum_temp += i
+        for j in range(i+1,n):
+            sum_temp += j
+            if sum_temp == n:
                 return True
+            elif sum_temp > n:
+                sum_temp = 0
+                continue
     return False
-
-
 # 获取输入数字n
 n = int(input())
 
-# 调用函数 
+# 调用函数
 print(check_consecutive_sum(n))

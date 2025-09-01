4a2628d370e0b6d633ac22dd6ca2923faf039b0f
L. Mingkai <704021302@qq.com>
2025-09-01 20:54:17 +0800

    Update 076.py

--- 076.py
+++ 076.py
@@ -1,13 +1,11 @@
 def min_removals_to_anagram(str1, str2):
     # 此处编写代码
-    lst1 = list(str1)
-    lst2 = list(str2)
+    list1, list2 = list(str1), list(str2)
     for letter in str1:
         if letter in str2:
-            lst1.remove(letter)
-            lst2.remove(letter)
-    return len(lst1) + len(lst2)
-
+            list1.remove(letter)
+            list2.remove(letter)
+    return len(list1)+len(list2)
 # 获取输入 
 str1 = input()
 str2 = input()

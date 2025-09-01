2c98c5dad7bc9648c02277fa31e3ffb9649fe9b5
L. Mingkai <704021302@qq.com>
2025-09-01 14:45:44 +0800

    Update 066.py

--- 066.py
+++ 066.py
@@ -1,13 +1,9 @@
 def is_string_palindrome(string):
     # 此处编写代码
-    if len(string) < 2:
+    if len(string) < 2 or string == string[::-1]:
         return True
     else:
-        if string[0] == string[-1]:
-            return is_string_palindrome(string[1:-1])
-        else:
-            return False
-
+        return False
 # 获取用户输入
 user_input = input()
 

e2d7a36b52e74730cfa225a1d52526d29aff6751
L. Mingkai <704021302@qq.com>
2025-09-01 15:21:14 +0800

    Update 072.py

--- 072.py
+++ 072.py
@@ -1,13 +1,8 @@
 def sort_by_last_char(sentence):
      # 此处编写代码
-     words_lst = sentence.split(" ")
-     for i in range(len(words_lst)):
-          words_lst[i] = words_lst[i][::-1]
-     words_lst = sorted(words_lst)
-     for i in range(len(words_lst)):
-          words_lst[i] = words_lst[i][::-1]
-     return " ".join(words_lst)
-     
+     word_list = sentence.split(" ")
+     sorted_list = sorted(word_list, key = lambda x: x[-1])
+     return " ".join(sorted_list)
 # 输入句子 
 sentence = input()
 

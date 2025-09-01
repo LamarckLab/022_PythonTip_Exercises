c1d8711da57be0a1f8cde7e6af32e063cc35909d
L. Mingkai <704021302@qq.com>
2025-09-01 20:45:02 +0800

    Update 075.py

--- 075.py
+++ 075.py
@@ -1,12 +1,11 @@
 def convert_str_list_to_dict(str_list):
     # 此处编写代码
     output_dict = {}
-    string_lst = str_list.split(" ")
-    for element in string_lst:
-        equal_index = element.index("=")
-        output_dict[element[:equal_index]] = element[equal_index+1:]
+    element_list = str_list.split(" ")
+    for element in element_list:
+        key, value = element.split("=")
+        output_dict[key] = value
     return output_dict
-
 # 输入字符串 
 str_list = input()
 

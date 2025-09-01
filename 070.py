8f0250dc7525936488e4fb9fc6b4b10fdcc63cce
L. Mingkai <704021302@qq.com>
2025-09-01 14:55:15 +0800

    Update 070.py

--- 070.py
+++ 070.py
@@ -1,13 +1,12 @@
 def count_frequency(lst):
     # 此处编写代码
-    count_dict = {}
-    for element in lst:
-        if element not in count_dict:
-            count_dict[element] = 1
+    frequency_dict = {}
+    for word in lst:
+        if word not in frequency_dict:
+            frequency_dict[word] = 1
         else:
-            count_dict[element] += 1
-    return count_dict
-
+            frequency_dict[word] += 1
+    return frequency_dict 
 # 获取用户输入 
 lst = list(input().split())
 

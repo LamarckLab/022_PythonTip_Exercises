cde487e5bfc0d273fa61c6a0a019a2426f5cc4a2
L. Mingkai <704021302@qq.com>
2025-09-01 20:12:05 +0800

    Update 073.py

--- 073.py
+++ 073.py
@@ -1,10 +1,9 @@
 def can_form_target(list_of_lists, target_list):
     # 在这里编写你的代码
-    sum_lists = []
+    sum_list = []
     for sub_list in list_of_lists:
-        sum_lists += sub_list
-    return sorted(sum_lists) == sorted(target_list)
-
+        sum_list += sub_list
+    return sorted(sum_list) == sorted(target_list)
 # 获取用户输入
 user_list_of_lists = [list(map(int, input().split())) for _ in range(int(input()))]
 user_target_list = list(map(int, input().split()))

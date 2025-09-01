621e2f37cb79aa6968fb058ebd0b75c9bd1b93ba
L. Mingkai <704021302@qq.com>
2025-09-01 11:26:25 +0800

    Update 061.py

--- 061.py
+++ 061.py
@@ -1,11 +1,6 @@
 def find_common_elements(list1, list2):
-    # 此处编写代码
-    output_lst = []
-    for element in list1:
-        if element in list2:
-            output_lst.append(element)
-    return sorted(output_lst)
-
+     # 此处编写代码
+     return sorted(list((set(list1) & set(list2))))
 # 获取用户输入，转换为列表
 list1 = list(map(int, input().split()))
 list2 = list(map(int, input().split()))

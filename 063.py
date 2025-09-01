8cb8aee4d90f1d89085725a9a02668487eb4105b
L. Mingkai <704021302@qq.com>
2025-09-01 11:47:04 +0800

    Update 063.py

--- 063.py
+++ 063.py
@@ -1,13 +1,10 @@
 def max_gap(lst):
     # 此处编写代码
     max_gap = 0
-    lst = sorted(lst)
-    for i in range(len(lst)-1):
-        if lst[i+1] - lst[i] > max_gap:
-            max_gap = lst[i+1] - lst[i]
+    sorted_lst = sorted(lst)
+    for index in range(len(lst)-1):
+        max_gap = max(max_gap, sorted_lst[index+1] - sorted_lst[index])
     return max_gap
-
-
 # 获取用户输入，转换为整数列表
 numbers = list(map(int, input().split()))
 # 调用函数，输出结果

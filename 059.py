53435490c68206c0ec3b04120d98106c2f06a2cb
L. Mingkai <704021302@qq.com>
2025-09-01 11:16:27 +0800

    Update 059.py

--- 059.py
+++ 059.py
@@ -1,22 +1,22 @@
 def count_consonants(word):
     # 在此处编写你的代码
-    consonants_str ="bcdfghjklmnpqrstvwxyz"
-    consonants_count = 0
+    consonants_cnt = 0
     for letter in word.lower():
-        if letter in consonants_str:
-            consonants_count += 1
-    return consonants_count
+        if letter in consonants:
+            consonants_cnt += 1
+    return consonants_cnt
 
 def count_vowels(word):
     # 在此处编写你的代码
-    vowels_str = "aeiou"
-    vowels_count = 0
+    vowels_cnt = 0
     for letter in word.lower():
-        if letter in vowels_str:
-            vowels_count += 1
-    return vowels_count
+        if letter in vowels:
+            vowels_cnt += 1
+    return vowels_cnt
 
 # 获取用户输入
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
 word = input()
 
 # 调用函数
